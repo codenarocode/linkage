52d4282ff0d719639534fb5d2b79a009bdff61bb
Narottam Narhari <narottamishra67@gmail.com>
2021-10-03 01:56:12 +0530

    Added alot of new features

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 let myLinks=[]
 const inputEl = document.querySelector("#input-el")
+const inputName= document.querySelector("#input-name")
 const inputBtn=document.querySelector("#input-btn")
 const ulEl= document.querySelector("#ul-el")
 const deleteBtn=document.querySelector("#delete-btn")
 const  tabBtn=document.querySelector("#tab-btn")
+const warn=document.querySelector("h5")
 const storedItem = JSON.parse(localStorage.getItem("myLinks"))
 if(storedItem){
     myLinks= storedItem
     render(myLinks)
+    deleteBtn.style.display='block'
 }
 
 tabBtn.addEventListener("click", function(){
     chrome.tabs.query({active: true , currentWindow: true}, function(tabs){
-        myLinks.push(tabs[0].url)
-        localStorage.setItem("myLinks" , JSON.stringify(myLinks))
-        render(myLinks)
-    })
+       inputEl.value=tabs[0].url
+     })
     
 })
 
@@ -24,13 +25,8 @@ function render(links){
     let listItems=""
 
     for(let i=0;i<links.length;i++){
-     listItems+= `
-     <li>
-      <a target="_blank" href="${links[i]}"> ${links[i]} </a>
-     </li>
-     `
-      
-    }
+     listItems+= `<li>${links[i]}</li>`
+      }
     ulEl.innerHTML= listItems
     
 }
@@ -40,13 +36,38 @@ deleteBtn.addEventListener("dblclick", function(){
     localStorage.clear();
     myLinks=[]
     ulEl.innerHTML=""
+    deleteBtn.style.display='none'
 })
 
+
 inputBtn.addEventListener("click",function(){
-    let content= inputEl.value 
+    let name= inputName.value
+    let link= inputEl.value
+if(!link || !name){
+     if(!link){
+        inputEl.placeholder= "*Please add URL"
+        inputEl.classList.add('change-color')
+        
+     }
+
+     if(!name){
+        inputName.placeholder= "*Please add name"
+        inputName.classList.add('change-color')
+        
+     }
+    return
+}
+    inputEl.placeholder="URL"
+    inputName.placeholder="Name"
+    inputEl.classList.remove('change-color')
+    inputName.classList.remove('change-color')
+    let content= ` <a target="_blank" href="${link}">${name}</a>`
     myLinks.push(content)
+    inputName.value=''
     inputEl.value=''
-   
     localStorage.setItem("myLinks" ,  JSON.stringify(myLinks))
+    if(myLinks.length===1){
+        deleteBtn.style.display='block'
+    }
     render(myLinks)
 })
